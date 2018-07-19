ad71b0ee0520e82ac57deaa951db2c1f751561e9
jfederer <jfederer@usgs.gov>
2018-07-19 13:56:45 -0500

    Dashboard made slightly more pretty

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { styles } from '../style';
 import { Link } from 'react-router-dom';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+	root: {
+	  flexGrow: 1,
+	},
+	paper: {
+	  padding: theme.spacing.unit * 2,
+	  textAlign: 'center',
+	  color: theme.palette.text.secondary,
+	},
+  });
 
 class Dashboard extends React.Component {
 	componentDidMount() {
@@ -10,22 +23,28 @@ class Dashboard extends React.Component {
 	}
 
 	render() {
-
+		const { classes } = this.props;
 		return (
-			<div>
-				<h1>Dashboard</h1>
-				<p>General info and direct links to items (??? - need help here, this isn't in MN workflow)</p>
-				<ul>
-					<li><b>Number of unsubmitted events</b> - list with links to submit? red for ones that don't pass verification?</li>
-					<li><b>Event Manager</b>Total number of events - click for more info</li>
-					<li><b>Create/Start New Sampling Event</b><Link to='/FieldForm'><button onClick={()=>	{
+			<div className={classes.root}>
+			<h1>Dashboard</h1>
+			<p>General info and direct links to items (??? - need help here, this isn't in MN workflow)</p>
+			<Grid container spacing={24}>
+			  <Grid item xs>
+				<Paper className={classes.paper}><b>Number of unsubmitted events</b> - list with links to submit? red for ones that don't pass verification?</Paper>
+			  </Grid>
+			  <Grid item xs>
+				<Paper className={classes.paper}><b>Event Manager</b>Total number of events - click for more info</Paper>
+			  </Grid>
+			</Grid>
+			<Grid container spacing={24}>
+			  <Grid item xs>
+				<Paper className={classes.paper}><b>Create/Start New Sampling Event</b><br /><Link to='/FieldForm'><Button onClick={()=>	{
 						this.props.navControl("Water Quality",true);
 						this.props.navControl("Field Form",true);
-						}}>Start</button></Link></li>
-					<li><b>????</b></li>
-				</ul>
-			</div>
-
+						}}>Start</Button></Link></Paper>
+			  </Grid>
+			</Grid>
+		  </div>
 		);
 	}
 }
