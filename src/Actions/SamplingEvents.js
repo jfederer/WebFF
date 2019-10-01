7fa0336f2b1d1067cb075e99f98deb18b014a5aa
jfederer <jfederer@usgs.gov>
2019-10-01 16:01:38 -0500

    Bugfix:  Entering 1 on new table would show warning of shrink.  Also, shrinking would clear the last-most line regardless of need.  Both fixed.

--- src/Actions/SamplingEvents.js
+++ src/Actions/SamplingEvents.js
@@ -237,9 +237,11 @@ export function stationNameChanged(eventID, newStationName) {
 
 export function numberOfSamplingPointsChanged(eventID, sedimentType, setName, samplingMethod, numPoints, setInfoChangeHandler) {
 	//  console.log("numberOfSamplingPointsChanged(", eventID, sedimentType, setName, samplingMethod, numPoints, ")");
-	if (numPoints === null || numPoints === "" || isNaN(numPoints)) {
+	if (numPoints === null || numPoints === "" || parseInt(numPoints) === 0 || isNaN(numPoints)) {
 		return { type: 'CANCEL numberOfSamplingPointsChanged due to invalid numPoints passed' };
 	}
+
+
 
 	return dispatch => {
 		dispatch(showNavigationTab("QWDATA"));
@@ -247,31 +249,36 @@ export function numberOfSamplingPointsChanged(eventID, sedimentType, setName, sa
 
 		////// modify setInfo table //////
 		// make it the correct size (confirm with user if shrinking)
-		let setInfoSampleTableValue = getQuestionValue(eventID, DATA_ENTRY_INFORMATION_IDENTIFIER + sedimentType, DATA_ENTRY_INFORMATION_IDENTIFIER + sedimentType + IDENTIFIER_SPLITTER + SET_INFORMATION_IDENTIFIER + setName, "samplesTable_" + getMethodCategoryFromValue(samplingMethod));
+		let setInfoSampleTableValue = 
+				getQuestionValue(eventID, 
+								DATA_ENTRY_INFORMATION_IDENTIFIER + sedimentType, 
+								DATA_ENTRY_INFORMATION_IDENTIFIER + sedimentType + IDENTIFIER_SPLITTER + SET_INFORMATION_IDENTIFIER + setName, 
+								"samplesTable_" + getMethodCategoryFromValue(samplingMethod));
 
 		if (typeof setInfoSampleTableValue === 'undefined' || setInfoSampleTableValue === null) {
 			// eslint-disable-next-line no-useless-concat
 			let errMSG = "getQuestionValue(" + eventID + ", " + DATA_ENTRY_INFORMATION_IDENTIFIER + sedimentType + ", " + DATA_ENTRY_INFORMATION_IDENTIFIER + sedimentType + IDENTIFIER_SPLITTER + SET_INFORMATION_IDENTIFIER + setName + ", " + "samplesTable_" + getMethodCategoryFromValue(samplingMethod) + ") returned ";
 			errMSG += setInfoSampleTableValue === null ? "null" : "undefined";
-
 			throw new Error(errMSG);
 		}
 
 		//TODO: Decide when/how to react and/or confirm ... change stationing without notice? delete without notice if empty other than stationing, etc...
 
-		if (setInfoSampleTableValue.length >= parseInt(numPoints) + 1) {
+		
+
+		if (setInfoSampleTableValue.length > parseInt(numPoints) + 1) {
+			
 			// table must shrink
 			// console.log("Table must shrink");
 
-			// if the removed tables are empty... no worries, just do it.
-			//TODO:4
-
+			//TODO: // if the removed tables are empty... no worries, just do it.
+			
 			// otherwise, confirm before proceeding...
 
 			let deletionconfirmed = window.confirm("This will result in removing rows containing data from the Data Entry Set Information Data Table, QWDATA table, and Parameters Table... \n\n Do you want to continue?");
 
 			if (deletionconfirmed === true) {
-				while (setInfoSampleTableValue.length > numPoints) {
+				while (setInfoSampleTableValue.length > parseInt(numPoints)+1) {
 					setInfoSampleTableValue.pop();
 				}
 			} else {
@@ -281,19 +288,18 @@ export function numberOfSamplingPointsChanged(eventID, sedimentType, setName, sa
 		}
 
 
-		if (setInfoSampleTableValue.length <= numPoints) {
+		if (setInfoSampleTableValue.length < parseInt(numPoints) + 1) {
 			//table must expand
 			//TODO: warn stationing could change...
-			while (setInfoSampleTableValue.length <= numPoints) {
+			while (setInfoSampleTableValue.length < parseInt(numPoints) + 1) {
 				let newRow = new Array(setInfoSampleTableValue[0].length).fill("");
 				setInfoSampleTableValue.push(newRow);
 			}
-
-			setInfoChangeHandler(eventID, "samplesTable_" + getMethodCategoryFromValue(samplingMethod), setInfoSampleTableValue);  //TODO: underscore should be something defined in config
 		}
 
+		setInfoChangeHandler(eventID, "samplesTable_" + getMethodCategoryFromValue(samplingMethod), setInfoSampleTableValue);  //TODO: underscore should be something defined in config
 
-		// re-do any distance data  if EWI (confirm with user)
+		// re-do any distance data  if EWI (confirm with user) //TODO:
 
 		////// modify QWDATA table //////TODO:
 		let origQWDATAValue = getQuestionValue(eventID, QWDATA_TABLE_IDENTIFIER + sedimentType);
